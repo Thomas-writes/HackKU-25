5c1c1cc10d0e285511e240393313a82359cc5aa1
Daniel Budiman <143231650+Revenarts@users.noreply.github.com>
2025-04-05 10:56:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
-<<<<<<< HEAD
 
 import { GoogleGenAI } from 'https://cdn.jsdelivr.net/npm/@google/genai@latest/+esm';
 
 //create gemini AI
 const ai = new GoogleGenAI({ apiKey: "API_KEY" });
-=======
-import { GoogleGenAI } from 'https://cdn.jsdelivr.net/npm/@google/genai@latest/+esm';
-
-//create gemini AI
-const ai = new GoogleGenAI({ apiKey: "AIzaSyBAJI1McWcZ7i-ZSKk4BLhI0vFD1Yqy5O8" });
->>>>>>> 92c2be367119cf1da563bb6b717e4c84d7bb68a4
 
 //Gets song inside input box and sends to gemini
 async function getSong() 
 {
   const song_name = document.getElementById("in").value;
-<<<<<<< HEAD
-=======
-  document.getElementById("in").value = "";
->>>>>>> 92c2be367119cf1da563bb6b717e4c84d7bb68a4
   const response = await ai.models.generateContent(
   {
     model: "gemini-2.0-flash",
