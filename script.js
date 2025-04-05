b14b4d83777fe7072e913ba3fd4435edff495f97
Revenarts <budimanatwork@gmail.com>
2025-04-05 13:17:35 -0500

    added Advanced Filter

--- script.js
+++ script.js
@@ -1,22 +1,68 @@
 
+
 import { GoogleGenAI } from 'https://cdn.jsdelivr.net/npm/@google/genai@latest/+esm';
 
 //create gemini AI
-const ai = new GoogleGenAI({ apiKey: "API_KEY" });
+const ai = new GoogleGenAI({ apiKey: "AIzaSyBp9yVHCUpmaJVQF6ksR6fgzqS0uogfn7U" });
+
 
 //Gets song inside input box and sends to gemini
 async function getSong() 
 {
+  let response;
   const song_name = document.getElementById("in").value;
   document.getElementById("in").value = "";
+<<<<<<< Updated upstream
   const response = await ai.models.generateContent(
+=======
+  const adv_search = document.getElementById("adv_search");
+  if(adv_search.style.display == "none") 
+>>>>>>> Stashed changes
   {
-    model: "gemini-2.0-flash",
-    contents: "List 10 songs similar to " + song_name + " with no description"
-  });
+    response = await ai.models.generateContent(
+    {
+      model: "gemini-2.0-flash",
+      contents: "List 10 songs similar to " + song_name + " with no description"
+    });
+  }
+  else
+  {
+    console.log(document.getElementsByClassName("slider"));
+    const num_songs = document.getElementById("NUM").value;
+    console.log(num_songs);
+    const BPM = document.getElementById("BPM").value;
+    const duration = document.getElementById("DUR").value;
+    response = await ai.models.generateContent(
+      {
+        model: "gemini-2.0-flash",
+        contents: "List " + num_songs +" songs similar to " + song_name + " that are within " + BPM + " BPM and are around " + duration + 
+        " minutes long, with no description."
+      });
+  }
   let list = response.text;
-  list = list.split("\n").slice(2, 12);
+  console.log(list);
+  /*
+  response = await ai.models.generateContent(
+    {
+      model: "gemini-2.0-flash",
+      contents: "Remove all songs in the list by the author of the first song please and replace them with other songs. Here is the list: " + list
+    }
+  )
+  console.log(response.text);*/
+  list = list.split("\n").slice(2, 2+(document.getElementById("NUM").value));
   console.log(list);
 }
 
+function advSearch()
+{
+  const ele = document.getElementById("adv_search")
+  if(ele.style.display == "none"){
+   ele.style.display = "block";
+  }
+  else{
+    ele.style.display = "none";
+  }
+}
+
 document.getSong = getSong;
+document.advSearch = advSearch;
