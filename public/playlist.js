a8cd498314568ec61991f9d0dac9cad0350f2e49
Thomas Savasten <thomas@savasten.com>
2025-04-06 03:37:32 -0500

    a

--- public/playlist.js
+++ public/playlist.js
@@ -23,10 +23,6 @@ document.addEventListener("DOMContentLoaded", () => {
   if (window.location.search.includes("code=") && !sessionStorage.getItem("playlist_created")) {
     sessionStorage.setItem("playlist_created", "true");
 
-    // Clean up the URL
-    const cleanUrl = window.location.origin + window.location.pathname;
-    window.history.replaceState({}, document.title, cleanUrl);
-
     console.log("Handling redirect from Spotify");
     handleRedirectAndCreate();
   }
@@ -50,8 +46,10 @@ async function generateCodeChallenge(codeVerifier) {
 }
 
 async function handleRedirectAndCreate() {
+  const cleanUrl = window.location.origin + window.location.pathname;
+  window.history.replaceState({}, document.title, cleanUrl);
   const code = new URLSearchParams(window.location.search).get("code");
-const verifier = localStorage.getItem("code_verifier");
+  const verifier = localStorage.getItem("code_verifier");
 
 console.log("Code from URL:", code);
 console.log("Verifier from storage:", verifier);
